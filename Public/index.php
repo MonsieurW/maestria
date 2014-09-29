a6262624b5ea4e52a66d0acb9a7d88fa64e659bc
camael24 <thehawk@hoa-project.net>
2014-09-29 19:38:28 +0200

    up to tag

--- Public/index.php
+++ Public/index.php
@@ -3,8 +3,6 @@ namespace {
     require_once __DIR__ . '/../vendor/autoload.php';
     require_once __DIR__ .'/../Application/Maestria/Maestria.php'; // Need to use autoload xD
 
-    exit;
-
     try {
         $framework = new \Application\Maestria\Maestria();
         $framework->kit('redirector', new \Application\Controller\Kit\Redirection());
