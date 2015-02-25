dc59f930ab36bf3138444dbb08851b317fc4b188
camael24 <thehawk@hoa-project.net>
2015-02-25 07:37:10 +0100

    Mock/Redirect

--- Tests/Unit/Mock/Redirect.php
+++ Tests/Unit/Mock/Redirect.php
@@ -1,11 +1,20 @@
 <?php
 namespace Camael\Api\Tests\Unit\Mock;
 
-class Redirect extends \Sohoa\Framework\Kit\Redirector
+class Redirect extends \Application\Controller\Kit\Redirection
 {
-	public function redirect() {
-		
-	}
-   
+	public function redirect($ruleId, array $data = array(), $secured = null, $status = 302)
+    {
+        $uri = $this->router->unroute($ruleId, $data, $secured);
+
+        $response = $this->view->getOutputStream();
+        $response->sendHeader('Location', $uri, true, $status);
+    }
+
+    public function url($uri, $status = 302)
+    {
+        $response = $this->view->getOutputStream();
+        $response->sendHeader('Location', $uri, true, $status);
+    }
 
 }
