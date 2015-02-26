e1819a0188c03b00f8d839061d444ed3f4b94db3
camael24 <thehawk@hoa-project.net>
2015-02-26 14:26:28 +0100

    Use default redirector kit

--- Tests/Unit/Mock/Redirect.php
+++ Tests/Unit/Mock/Redirect.php
@@ -3,14 +3,6 @@ namespace Camael\Api\Tests\Unit\Mock;
 
 class Redirect extends \Application\Controller\Kit\Redirection
 {
-	public function redirect($ruleId, array $data = array(), $secured = null, $status = 302)
-    {
-        exit;
-    }
 
-    public function url($uri, $status = 302)
-    {
-        var_dump($ruleId);
-    }
 
 }
