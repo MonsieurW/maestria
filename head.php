c48ce5335b865414c08d57d1d57186206bd79b7e
MonsieurW <adressedallan@gmail.com>
2015-03-16 01:30:14 +0100

    Create head.php
    head and header

---
+++ head.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+
+<HTML>
+   <HEAD>
+      <TITLE>Maestria</TITLE>
+	  <link href="maestria.css" rel="stylesheet" type="text/css">
+	  <link href="../commun/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	  <link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
+	  <link href='print.css' rel='stylesheet' type='text/css' media="print">
+   </HEAD>
+   <BODY>
+      <header>
+	  <a href="index.html">Maestria</a>  Evaluer c'est connaître et comprendre c'est progresser
+		<div id="logzone">
+			<div class="login">Albert Einstein</div>
+			<div class="logout">DECONNEXION</div>
+		</div>
+	  </header>
+	  <nav>
+	
+		<h3><a href="classes.html">CLASSES</a></h3> 
+		<h3 class="synthese"><a href="synthese.html">SYNTHESE</a></h3>
+		<br/>
+		<h3><a href="evals.html">EVALUATIONS</a></h3>
+		<h3 id="evalchx" class="eval">PUISSANCE</h3><div class="flechebas"></div>
+		<h3 class="eval"><a href="correction.html">CORRECTION</a></h3>
+		<br/>
+		<h3><a href="items.html">ITEMS PEDA</a></h3>
+		
+		<footer>
+			<a href="">Contact</a>
+			<a href="http://metaphysik.fr">Metaphysik</a>
+		</footer>	
+	  </nav>
+		<section id="popup">		
+			<section id="inpopup">
+			
+		</section>
+	  </section>
